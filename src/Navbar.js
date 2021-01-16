29f6eabb5584914b68921bb60252b719740a3860
Edwin Torres <edwin_torres@icloud.com>
2021-01-15 23:02:55 -0500

    Initial

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Edwin's Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog Post</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
