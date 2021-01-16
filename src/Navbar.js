b42597345c926d73504388d5abbeb0f1b8c1ea17
Edwin Torres <edwin_torres@icloud.com>
2021-01-16 01:25:23 -0500

    Setting up blog structure.

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>Edwin's Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog Post</a>
+                <a href="/create">New Blog </a>
             </div>
         </nav>
      );
