d3b4b018d9b7d0e2899b36344a779200c28e4e18
Edwin Torres <edwin_torres@icloud.com>
2021-01-18 03:34:49 -0500

    Updated project with new formatting convention.

--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,30 @@ import BlogList from './BlogList'
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+	const [blogs, setBlogs] = useState([
+		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+		{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+	])
 
-    const [name, setName] = useState('mario')
+	const [name, setName] = useState('mario')
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter(blog => blog.id !== id)
+		setBlogs(newBlogs)
+	}
 
-    useEffect(() => {
+	useEffect(() => {
 
-    }, [])
+	}, [])
 
-    return (
-        <div className="Home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <button onClick={() => setName('luigi')}>Change Name</button>
-            <p>{name}</p>
-        </div>
-     );
+	return (
+		<div className="Home">
+			<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+			<button onClick={() => setName('luigi')}>Change Name</button>
+			<p>{name}</p>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
