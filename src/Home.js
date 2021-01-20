c7aa25989e3e31bce717adae56766fd6abf794fc
Edwin Torres <edwin_torres@icloud.com>
2021-01-19 23:28:07 -0500

    Added fetch requests and removed handleDelete to be implemeted again later.

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,23 @@ import BlogList from './BlogList'
 
 const Home = () => {
 
-	const [blogs, setBlogs] = useState([
-		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-		{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-	])
+	const [blogs, setBlogs] = useState(null)
 
 	const [name, setName] = useState('mario')
 
-	const handleDelete = (id) => {
-		const newBlogs = blogs.filter(blog => blog.id !== id)
-		setBlogs(newBlogs)
-	}
-
 	useEffect(() => {
-
+		fetch('http://localhost:8000/blogs')
+			.then(res => {
+				return res.json()
+			})
+			.then(data => {
+				setBlogs(data)
+			})
 	}, [])
 
 	return (
 		<div className="Home">
-			<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+			{ blogs && <BlogList blogs={blogs} title="All Blogs!" />}
 			<button onClick={() => setName('luigi')}>Change Name</button>
 			<p>{name}</p>
 		</div>
